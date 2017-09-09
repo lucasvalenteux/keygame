cc3f67107d0999017a36ddef0066a4ddfa6746fa
Lucas <luckvalente@gmail.com>
2017-09-09 19:55:27 -0300

    Área logada

---
+++ home.php
@@ -0,0 +1,24 @@
+<html>
+    
+    <head>
+        <title>Keygame</title>
+        <meta name="description" content="Página pessoal e de projetos sobre o desenvolvedor Lucas Valente. ">            
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estiloindex.css">        
+    </head>
+    
+    <body>
+       
+      <div class="center topo">
+          <p>Logado</p>
+      </div>
+
+
+
+      <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    </body>
+    
+    </html>
